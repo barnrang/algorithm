3ef12477453b520cfaa0287c7d09477b17bb1e85
NAPAT <barnrang@NAPATs-MacBook-Air.local>
2018-03-05 03:36:06 +0900

    add floyd

--- graph/Shortest_path/Floyd/Floyd.cpp
+++ graph/Shortest_path/Floyd/Floyd.cpp
@@ -9,12 +9,45 @@ y1 y2 ... y_(V^2) <square shape>
 V - vertices
 Complexity: O(V^3)
 */
-
-
 #include <iostream>
 #include <algorithm>
 #include <vector>
+using namespace std;
+#define INF 0x3f3f3f3f
+int V;
+long graph[1000][1000];
+
+void floyd();
 
 int main(){
+    cin >> V;
+    for(int i = 0; i < V; i++){
+        for(int j = 0; j < V; j++){
+            cin >> graph[i][j];
+        }
+    }
+    floyd();
+    return 0;
+}
 
+
+/*
+Return:
+2-D Matrix
+*/
+
+void floyd(){
+    for(int k = 0; k < V; k++){
+        for(int i = 0; i < V; i++){
+            for(int j = 0; j < V; j++){
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+            }
+        }
+    }
+    for(int i = 0; i < V; i++){
+        for(int j = 0; j < V; j++){
+            cout << graph[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }
